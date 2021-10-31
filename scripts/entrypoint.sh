e2e972c6081cb2d8f131903a7a52690e4a4cf01a
Paul Schifferer <paul@schifferers.net>
2021-10-31 13:54:13 -0700

    Add NewRelic

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,4 +3,4 @@
 set -e
 
 #newrelic-admin run-program gunicorn wsgi:app
-uwsgi --http :${PORT} --module wsgi:app --master --processes 4 --threads 2
+newrelic-admin run-program uwsgi --http :${PORT} --module wsgi:app --master --processes 4 --threads 2
