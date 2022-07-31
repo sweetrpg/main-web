a2b6b40ad007f2e2ae2ff0994f13082be55023bd
Paul Schifferer <paul@schifferers.net>
2022-07-31 16:33:07 -0700

    Update deployment

--- kubernetes/secrets/dev/deploy-secrets.sh
+++ kubernetes/secrets/dev/deploy-secrets.sh
@@ -16,10 +16,14 @@ kubectl create -n "${ns}" secret generic template-credentials \
     --from-env-file=../git-templates.env
 
 echo "Docker registry config..."
+# kubectl create -n "${ns}" secret docker-registry sweetrpg-registry \
+#     --docker-server=registry.sweetrpg.com \
+#     --docker-username=docker \
+#     --docker-password=ESU7PnNtlt07zkvbnSyByrTdzllajxIQWqY7mswQR78
 kubectl create -n "${ns}" secret docker-registry sweetrpg-registry \
-    --docker-server=registry.sweetrpg.com \
-    --docker-username=docker \
-    --docker-password=ESU7PnNtlt07zkvbnSyByrTdzllajxIQWqY7mswQR78
+    --docker-server=ghcr.io \
+    --docker-username=paulyhedral \
+    --docker-password=$(cat ${scriptdir}/tokens.txt | grep '^sweetrpg-registry' | cut -f2 -d:)
 #kubectl create -n "${ns}" secret dockerconfigjson sweetrpg-registry \
 #    --from-file=.dockerconfigjson
 
